eaaaaccdd5d6ed1259591da18716d93bd3863226
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-18 20:00:40 -0300

    feat: add transfer lambda infra

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -20,3 +20,26 @@ resource "aws_lambda_function" "invoices" {
     }
   }
 }
+
+data "archive_file" "transfers_lambda_zip" {
+  type        = "zip"
+  source_dir  = "../lambda/__compiled__/transfers"
+  output_path = "../lambda/__zip__/transfers.zip"
+}
+
+resource "aws_lambda_function" "transfers" {
+  function_name    = "stark-challenge-transfers"
+  filename         = data.archive_file.transfers_lambda_zip.output_path
+  source_code_hash = data.archive_file.transfers_lambda_zip.output_base64sha256
+  role             = aws_iam_role.invoke_lambda.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  timeout          = 10
+
+  environment {
+    variables = {
+      REGION     = "us-east-1"
+      PROJECT_ID = "6361128644902912"
+    }
+  }
+}
