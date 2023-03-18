18f0e40837957889ea25c9f3f28744cc1ae7fbda
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-18 20:00:28 -0300

    feat: add invoices lambda infra

---
+++ terraform/lambda.tf
@@ -0,0 +1,22 @@
+data "archive_file" "invoices_lambda_zip" {
+  type        = "zip"
+  source_dir  = "../lambda/__compiled__/invoices"
+  output_path = "../lambda/__zip__/invoices.zip"
+}
+
+resource "aws_lambda_function" "invoices" {
+  function_name    = "stark-challenge-invoices"
+  filename         = data.archive_file.invoices_lambda_zip.output_path
+  source_code_hash = data.archive_file.invoices_lambda_zip.output_base64sha256
+  role             = aws_iam_role.invoke_lambda.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  timeout          = 10
+
+  environment {
+    variables = {
+      REGION     = "us-east-1"
+      PROJECT_ID = "6361128644902912"
+    }
+  }
+}
