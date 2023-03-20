c798252744f86d068538fa241c48a28c6b646a1a
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-20 20:33:40 -0300

    test: ensure invoices lambda creates invoices and returns 200 http statusCode

--- lambda/typescript/invoices/invoices.spec.ts
+++ lambda/typescript/invoices/invoices.spec.ts
@@ -1,5 +1,19 @@
-describe('InvoiceTest', () => {
-	it('should run initial test config', () => {
-		expect(true).toBe(true);
+import { Context } from 'aws-lambda';
+import starkbank from 'starkbank';
+import { handler } from '.';
+
+const mockCallback = jest.fn();
+
+describe('Invoices Lambda', () => {
+	it('should create invoices and return 200 statusCode', async () => {
+		const invoiceCreateSpy = jest.spyOn(starkbank.invoice, 'create');
+
+		await handler({}, {} as Context, mockCallback);
+
+		expect(invoiceCreateSpy).toHaveBeenCalledTimes(1);
+		expect(mockCallback).toHaveBeenCalledWith(null, {
+			statusCode: 200,
+			body: true,
+		});
 	});
 });
