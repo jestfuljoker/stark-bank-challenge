dbe96f8797a555c61a5b05617a4c04258b51fc04
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-20 20:55:45 -0300

    test: ensure createInvoices function create an array with valid amounts

--- lambda/typescript/invoices/helpers/createInvoices.spec.ts
+++ lambda/typescript/invoices/helpers/createInvoices.spec.ts
@@ -7,4 +7,13 @@ describe('createInvoices helper function', () => {
 		expect(invoices.length).toBeGreaterThanOrEqual(8);
 		expect(invoices.length).toBeLessThanOrEqual(12);
 	});
+
+	it('should create an array of invoices with valid amounts', () => {
+		const invoices = createInvoices();
+
+		for (const invoice of invoices) {
+			expect(invoice.amount).toBeGreaterThanOrEqual(50);
+			expect(invoice.amount).toBeLessThanOrEqual(2000);
+		}
+	});
 });
