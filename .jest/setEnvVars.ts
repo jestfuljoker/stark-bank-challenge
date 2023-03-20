4c5b590c3c96b2eb6ba2bb2a8c0759eb6e6a1d1b
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-19 21:36:07 -0300

    refactor: move private pem content to jest env var

--- .jest/setEnvVars.ts
+++ .jest/setEnvVars.ts
@@ -1,1 +1,12 @@
 process.env.PROJECT_ID = '5656565656565656';
+// This is a fake privatePemKey value
+process.env.PRIVATE_PEM = `
+-----BEGIN EC PARAMETERS-----
+BgUrgQQACg==
+-----END EC PARAMETERS-----
+-----BEGIN EC PRIVATE KEY-----
+MHQCAQEEIMCwW74H6egQkTiz87WDvLNm7fK/cA+ctA2vg/bbHx3woAcGBSuBBAAK
+oUQDQgAE0iaeEHEgr3oTbCfh8U2L+r7zoaeOX964xaAnND5jATGpD/tHec6Oe9U1
+IF16ZoTVt1FzZ8WkYQ3XomRD4HS13A==
+-----END EC PRIVATE KEY-----
+`;
