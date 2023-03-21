edeb3d5a943b978df5bc9cb38b02ce1144fed617
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-20 21:31:53 -0300

    test: ensure tranfers lambda returns 500 statusCode on transfer creation fails

--- lambda/typescript/transfers/transfers.spec.ts
+++ lambda/typescript/transfers/transfers.spec.ts
@@ -5,33 +5,46 @@ import starkbank from 'starkbank';
 
 const mockCallback = jest.fn();
 
-const body = {
-	event: {
-		log: {
-			invoice: {
-				status: 'paid',
-				amount: Number.parseInt(faker.finance.amount()),
+const event = {
+	body: JSON.stringify({
+		event: {
+			log: {
+				invoice: {
+					status: 'paid',
+					amount: Number.parseInt(faker.finance.amount()),
+				},
+				type: 'credited',
 			},
-			type: 'credited',
 		},
-	},
-};
+	}),
+} as APIGatewayProxyEvent;
 
 describe('transfers lambda', () => {
 	it('should create a transfer and return 200 statusCode', async () => {
 		const transfersCreateSpy = jest.spyOn(starkbank.transfer, 'create');
 
-		const event = {
-			body: JSON.stringify(body),
-		} as APIGatewayProxyEvent;
-
 		await handler(event, {} as Context, mockCallback);
 
 		expect(transfersCreateSpy).toHaveBeenCalledTimes(1);
-
 		expect(mockCallback).toBeCalledWith(null, {
 			statusCode: 200,
 			body: true,
+		});
+	});
+
+	it('should return 500 if transfer has an invalid id', async () => {
+		jest
+			.spyOn(starkbank.transfer, 'create')
+			.mockResolvedValue([{ id: '' }] as starkbank.Transfer[]);
+
+		await handler(event, {} as Context, mockCallback);
+
+		expect(mockCallback).toBeCalledWith('INTERNAL_ERROR', {
+			statusCode: 500,
+			body: JSON.stringify({
+				error: true,
+				message: 'An internal error occurred to generate transfers',
+			}),
 		});
 	});
 });
