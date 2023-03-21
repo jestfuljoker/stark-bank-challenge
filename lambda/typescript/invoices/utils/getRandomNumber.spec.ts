54614f0e236b6f5b9a3b493f1a7d6675bdabd4d3
Christofer Assis <chris.f.assis18@gmail.com>
2023-03-20 21:09:28 -0300

    test: ensure getRandomNumber generate different random numbers on multiple runs

--- lambda/typescript/invoices/utils/getRandomNumber.spec.ts
+++ lambda/typescript/invoices/utils/getRandomNumber.spec.ts
@@ -10,4 +10,13 @@ describe('getRandomNumber utils function', () => {
 		expect(randomNumber).toBeGreaterThanOrEqual(min);
 		expect(randomNumber).toBeLessThanOrEqual(max);
 	});
+
+	it('should generate different random numbers on multiple runs', () => {
+		const min = faker.datatype.number();
+		const max = faker.datatype.number({ min });
+		const randomNumber1 = getRandomNumber(min, max);
+		const randomNumber2 = getRandomNumber(min, max);
+
+		expect(randomNumber1).not.toBe(randomNumber2);
+	});
 });
